813519c2598e1986cee6b39102b3dfabe1b3a1d1
tekater <120014627+tekater@users.noreply.github.com>
2023-06-21 22:52:42 +0900

    Update del_add.cpp

--- del_add.cpp
+++ del_add.cpp
@@ -35,27 +35,59 @@ public:
 
     void delator(int pos) {
         columns--;
-        int* narr = new int[(rows) * columns];
+        int* narr = new int[(rows)*columns];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                    narr[index(i, j)] = arr[index(i, j + (j>=pos)+i)];
+                narr[index(i, j)] = arr[index(i, j + (j >= pos) + i)];
             }
         }
         delete[] arr;
         arr = narr;
     }
     void dobaver(int pos) {
-        
         int* narr = new int[rows * (columns + 1)] {};
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns+1; j++) {
-                    narr[index(i, j + (j >= pos) + i)] = arr[index(i, j)];
+            for (int j = 0; j < columns + 1; j++) {
+                narr[index(i, j + (j >= pos) + i)] = arr[index(i, j)];
             }
         }
-        
+
         delete[] arr;
         arr = narr;
         columns++;
+    }
+
+    void del(int pos) {
+        rows--;
+        int* narr = new int[(rows)*columns];
+        for (int i = 0,o = 0; i < rows; i++,o++) {
+            for (int j = 0; j < columns; j++) {
+                narr[index(i, j)] = arr[index(o, j)];
+                if (o == pos) {
+                    o++;
+                }
+            }
+        }
+        delete[] arr;
+        arr = narr;
+    }
+
+    void add(int pos) {
+        int* narr = new int[(rows+1)*columns];
+        for (int i = 0, o = 0; i < rows+1; i++, o++) {
+            for (int j = 0; j < columns; j++) {
+                if (i == pos) {
+                    narr[index(i, j)] = 0;
+                    o++;
+                }
+                else {
+                    narr[index(o, j)] = arr[index(i, j)];
+                }
+            }
+        }
+        delete[] arr;
+        arr = narr;
+        rows++;
     }
 
     void print()
@@ -79,5 +111,11 @@ int main()
     a.randomize();
     a.print();
     a.dobaver(2);
+    a.print();
+
+
+    a.del(2);
+    a.print();
+    a.add(3);
     a.print();
 }
